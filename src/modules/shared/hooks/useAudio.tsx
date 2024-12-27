86646bb4c26b71ca58ea2a25d59e8ae1230cfc6e
RojanSr <rairojan1116@gmail.com>
2024-12-27 23:22:55 +0545

    feat: multiple audio

--- src/modules/shared/hooks/useAudio.tsx
+++ src/modules/shared/hooks/useAudio.tsx
@@ -8,25 +8,53 @@ export type UseAudioReturnType = {
 };
 
 const useAudio = (
-  url: string,
-  config: { loop?: boolean }
+  url: string | string[],
+  config: { loop?: boolean; shuffle?: boolean }
 ): UseAudioReturnType => {
   const playing = usePokeStore((store) => store.value.isMusicPlaying);
   const setPlaying = usePokeStore((store) => store.setter.setIsMusicPlaying);
 
-  const [audio] = useState(new Audio(url));
+  const [audioIndex, setAudioIndex] = useState(
+    Array.isArray(url) && config.shuffle
+      ? Math.floor(Math.random() * url.length)
+      : 0
+  );
+  const [audio, setAudio] = useState<HTMLAudioElement>(
+    Array.isArray(url) ? new Audio(url[audioIndex]) : new Audio(url)
+  );
 
   const toggle = () => setPlaying(!playing);
+
+  const handleNextAudio = ({ play }: { play: boolean }) => {
+    const nextAudioIndex = audioIndex === url.length - 1 ? 0 : audioIndex + 1;
+    const nextAudio = new Audio(url[nextAudioIndex]);
+    setAudioIndex(nextAudioIndex);
+    setAudio(nextAudio);
+    if (play) {
+      nextAudio.play();
+    }
+  };
 
   const stopAndReset = () => {
     if (playing) {
       setPlaying(false);
       audio.pause();
       audio.currentTime = 0;
+      handleNextAudio({ play: false });
     }
   };
 
   function handleEnd() {
+    if (Array.isArray(url)) {
+      const reachedEnd = audioIndex === url.length - 1;
+      if (reachedEnd && !config.loop) {
+        setPlaying(false);
+        return;
+      }
+      handleNextAudio({ play: true });
+      return;
+    }
+
     if (config.loop) {
       audio.play();
     } else {
